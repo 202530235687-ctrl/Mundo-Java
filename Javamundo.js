c23659c3f663d81b1d02e46f71f5cb2370be091e
202530235687-ctrl <202530235687@estudante.rn.gov.br>
2026-07-16 17:11:43 -0300

    Update Javamundo.js
    adicionei mais algumas  frases do saiba mais

--- Javamundo.js
+++ Javamundo.js
@@ -8,6 +8,10 @@ const curiosidades = [
     "🧬 Os dinossauros foram criados usando DNA de mosquito preservado em âmbar.",
     "🏆 Jurassic Park ganhou 3 Oscars de efeitos visuais, som e edição de som.",
     "🦕 O Velociraptor do filme é bem maior do que os encontrados na vida real."
+    "A vida encontra um caminho. — Ian Malcolm",
+    "Se eles conseguiram fazer isso, será que deveriam?",
+    "Bem-vindos ao Jurassic Park. — John Hammond",
+    "Os cientistas estavam tão preocupados em saber se podiam que não pararam para pensar se deviam. — Ian Malcolm"
 ];
 
 const botao = document.getElementById("btnCuriosidade");
